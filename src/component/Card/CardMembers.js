f95efce1c901f548c2844696bc35fa5fcfb284fb
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 09:38:17 +0430

    delete commits and console logs and change src img

--- src/component/Card/CardMembers.js
+++ src/component/Card/CardMembers.js
@@ -1,7 +1,5 @@
 import { Col, Row } from "react-bootstrap";
 import "./cardMembers.css";
-// import { useEffect, useState } from "react";
-// import { getAllMembersAPI } from "../../API/membersAPI";
 import useAllMembers from "../../hooks/AllMembers/useAllMembers";
 import useAllTasks from "../../hooks/AllTasks/useAllTasks";
 import { useState, useEffect } from "react";
@@ -9,13 +7,10 @@ import useSearchMember from "../../hooks/SearchMember/useSearchMember";
 
 function CardMembers() {
   const searchName = useSearchMember().SearchName;
-  console.log(searchName);
-  // const dataAllMembers = useAllMembers();
   const AllTasks = useAllTasks().AllTasks;
   const allMembers = useAllMembers().AllMembers;
-  console.log(allMembers);
   const [AllMembers, setAllMembers] = useState([]);
-  // set all members if enter search name
+
   useEffect(() => {
     if (searchName !== "") {
       allMembers?.map((memberr) => {
@@ -32,51 +27,36 @@ function CardMembers() {
   const [classNameTasks, setClassNameTasks] = useState("");
 
   function GetClassNameTasks(titleOfTask) {
-    console.log(titleOfTask);
     AllTasks.filter((task) => task.title === titleOfTask).map((taskFilter) => {
       if (taskFilter.complete) {
         setClassNameTasks("green");
-        console.log("green");
       } else {
         setClassNameTasks("red");
-        console.log("red");
       }
-      console.log("filter map", taskFilter);
     });
   }
-  // } else {
-  // setAllMembers(AllMembersUseAllMember);
-  // }
-  // if (searchName !== "") {
-  //   setAllMembers(allMembers.map((member) => member.name.includes(searchName)));
-  // }
 
   return (
     <section className="container">
       <Row className="rowOfCards mt-4 ">
-        {/* <h6 onClick={handle}>cards</h6> */}
         {AllMembers.length > 0 ? (
           AllMembers?.map((member, index) => {
-            console.log("member.tasks");
             return (
               <Col
                 key={member.name + "colMember"}
                 xs="12"
                 md="6"
                 xl="4"
-                // xl="4"
                 className=" py-2 colOfCard "
               >
                 <div className="BoxMember">
                   <Row xs="12" md="6" className=" pe-md-5 imgBox">
                     <img
                       className="imgCard"
-                      src="/client/public/user.png"
+                      src="./user.png"
                       alt="personal image"
                     />
-                    {/* if(document.querySelectorAll(".red").length === member.tasks.length) {<span>red</span>} */}
                   </Row>
-                  {/* <p className="title">{titleName}</p> */}
                   <Row className="details ">
                     <Col xs="12" sm="6" className="details-col">
                       <p className="item-sub">
@@ -87,7 +67,7 @@ function CardMembers() {
                         Github: <span>{member.github}</span>
                       </p>
                       <p className="item-sub">
-                        Admin:{" "}
+                        Admin:
                         <span>{member.admin ? "admin" : "costomer"}</span>
                       </p>
                     </Col>
@@ -133,9 +113,6 @@ function CardMembers() {
                         <span className="item-sub "> Tasks: </span>
                         <div className="BoxSkills">
                           {member?.tasks?.map((titleOfTask) => {
-                            console.log("titleOfTask");
-                            // GetClassNameTasks(titleOfTask);
-
                             return AllTasks.filter(
                               (task) => task.title === titleOfTask
                             ).map((taskFilter) => {
@@ -148,7 +125,6 @@ function CardMembers() {
                                     {titleOfTask}
                                   </span>
                                 );
-                                // setClassNameTasks("green");
                               } else {
                                 return (
                                   <span
@@ -158,68 +134,8 @@ function CardMembers() {
                                     {titleOfTask}
                                   </span>
                                 );
-                                // setClassNameTasks("red");
                               }
                             });
-                            // return (
-                            //   <span
-                            //     key={titleOfTask}
-                            //     className={" spanItem mx-2 mb-2"}
-                            //   >
-                            //     {titleOfTask}
-                            //   </span>
-                            // );
-                            // {
-                            //   AllTasks.filter(
-                            //     (task) => task.title === titleOfTask
-                            //   ).map((taskFiltering) => {
-                            //     return taskFiltering.complete ? (
-                            //       <span
-                            //         key={index + titleOfTask}
-                            //         className="red spanItem mx-2 mb-2"
-                            //       >
-                            //         {titleOfTask}
-                            //       </span>
-                            //     ) : (
-                            //       <span
-                            //         key={index + titleOfTask}
-                            //         className="green spanItem mx-2 mb-2"
-                            //       >
-                            //         {titleOfTask}
-                            //       </span>
-                            //     );
-                            //   });
-                            // }
-
-                            // );
-                            // return AllTasks.map((taskOfalltask) => {
-                            //   if (
-                            //     taskOfalltask.title === titleOfTask &&
-                            //     taskOfalltask.complete
-                            //   ) {
-                            //     return (
-                            //       <span
-                            //         key={index}
-                            //         className="green spanItem mx-2 mb-2"
-                            //       >
-                            //         {task}
-                            //       </span>
-                            //     );
-                            //   } else if (
-                            //     taskOfalltask.title === task &&
-                            //     !taskOfalltask.complete
-                            //   ) {
-                            //     // setRedTasks(redTasks + 1);
-                            //     return (
-                            //       <span
-                            //         key={index}
-                            //         className="red spanItem mx-2 mb-2"
-                            //       >
-                            //         {task}
-                            //       </span>
-                            //     );
-                            //   }
-                            // });
                           })}
                         </div>
                       </p>
