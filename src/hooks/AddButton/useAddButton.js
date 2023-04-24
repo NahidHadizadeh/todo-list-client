80872efeffdf0fbd9d22a22a7d94c9ae32e3d948
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 20:34:42 +0430

    delete commits

--- src/hooks/AddButton/useAddButton.js
+++ src/hooks/AddButton/useAddButton.js
@@ -3,7 +3,6 @@ import { AddBtnContext } from "../../context/AddButtonContext";
 
 const useAddButton = () => {
   const data = useContext(AddBtnContext);
-  // console.log(data);
   return data;
 };
 export default useAddButton;
--- src/hooks/AllTasks/useAllTasks.js
+++ src/hooks/AllTasks/useAllTasks.js
@@ -1,9 +1,7 @@
 import { useContext } from "react";
 import { AllTasksContext } from "../../context/TasksContext";
-// import {AllTa}
 const useAllTasks = () => {
   const data = useContext(AllTasksContext);
-  //   console.log(data);
   return data;
 };
 export default useAllTasks;
