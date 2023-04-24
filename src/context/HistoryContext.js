5d0fc190d149f8e73d8170e9c1e690e7fcb5dadf
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 15:58:02 +0430

    delete commit and log

--- src/context/HistoryContext.js
+++ src/context/HistoryContext.js
@@ -1,19 +1,12 @@
 import { createContext, useState, useEffect } from "react";
-// import { getAllChangingAPI } from "../API/membersAPI";
 import { getAllHistoryAPI } from "../API/historyAPI";
 export const AllHistoryContext = createContext({
   AllHistory: [],
   setAllHistory: () => {},
-  // allHistory:[],
-  // setAllHistory:()=>{}
-  // setOneMember: () => {},
 });
 
 function HistoryProvider({ children }) {
-  //   const [ShowModal, setShowModal] = useState(false);
   const [AllHistory, setAllHistory] = useState([]);
-  // const [allHistory, setAllHistory] = useState([]);
-  console.log(AllHistory?.reverse());
   useEffect(() => {
     getAllHistoryAPI().then((res) => setAllHistory(res.data?.reverse()));
   }, []);
@@ -22,9 +15,6 @@ function HistoryProvider({ children }) {
       value={{
         AllHistory,
         setAllHistory,
-        // allHistory,
-        // setAllHistory,
-        // setOneMember,
       }}
     >
       {children}
@@ -32,4 +22,3 @@ function HistoryProvider({ children }) {
   );
 }
 export default HistoryProvider;
-// //////////////////////////////////
