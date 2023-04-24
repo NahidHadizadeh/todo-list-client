09649c436197eea5aa392425d6bde693ebfc4f36
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 20:58:50 +0430

    delete commits and change src of img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAllTodosAPI, deleteOneTodoAPI } from "../API/todoListAPI";
 import CreateTodo from "../component/CreateTodo";
-import { AiOutlineDelete, AiOutlineEdit, AiOutlineStar } from "react-icons/ai";
 import EditTodo from "../component/EditTodo";
-// import Timer from "../component/Timer/Timer";
 import NavbarProject from "../component/Navbar";
 import TableProj from "../component/TableProj";
 import AddButton from "../component/AddButton";
@@ -12,47 +10,15 @@ import CreateTask from "../component/CreateTodo";
 import useAddButton from "../hooks/AddButton/useAddButton";
 import useAllTasks from "../hooks/AllTasks/useAllTasks";
 import useAllMembers from "../hooks/AllMembers/useAllMembers";
-// import AddButton from "../component/AddButton/AddButton";
 
 function Home() {
   const dataBtn = useAddButton();
   const AllTasks = useAllTasks().AllTasks;
   const AllMembers = useAllMembers().AllMembers;
-  // console.log(AllTasks);
-  //
   const navigate = useNavigate();
   //// set showmodal value and show and hidden it
-  // const [ShowModal, setShowModal] = useState(false);
-  // const [ShowModalEdit, setShowModalEdit] = useState(false);
-  // const handleCloseModal = () => {
-  //   setShowModal(false);
-  //   navigate("/");
-  // };
-  // const handleCloseModalEdit = () => {
-  //   setShowModalEdit(false);
-  //   navigate("/");
-  // };
-  // const handleShowModal = () => setShowModal(true);
-  // const handleShowModalEdit = () => setShowModalEdit(true);
-
-  //// handle click for show modal and Add todo
-  // function handleAddTodo() {
-  //   handleShowModal();
-  //   navigate("/create");
-  // }
-  //// set All todos
-  // const [AllTodos, setAllTodos] = useState([]);
-
-  // useEffect(() => {
-  //   //// get all todos with API
-  //   getAllTodosAPI().then((res) => {
-  //     setAllTodos(res.data);
-  //   });
-  // }, []);
-  //// handle delete
 
   return (
-    // <h1>home</h1>
     <>
       <NavbarProject />
       <div className="container sectionbox">
@@ -66,7 +32,7 @@ function Home() {
                   return (
                     <div className="adminImage">
                       <img
-                        src="/client/public/user.png"
+                        src="./user.png"
                         alt={member.title}
                         title={member.title}
                       />
@@ -92,14 +58,6 @@ function Home() {
         </section>
         <h3>Todo List</h3>
         <div className="newTaskAndTotalBox">
-          {/* <button
-            id="addButton"
-            className="btn btn-dark "
-            onClick={handleAddTodo}
-          >
-            Add New Task
-          </button> */}
-          {/* <AddButton BtnText="Add New Task" /> */}
           <AddButton BtnText="Add New Task" />
           <label id="totalTasks" className="">
             total tasks : {AllTasks.length}
