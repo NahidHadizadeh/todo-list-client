6d64f7abd786a738ff1cae13a88055b3d4b6069e
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 10:02:34 +0430

    delete commit and edit upload btn

--- src/component/UploadFile/UploadFile.js
+++ src/component/UploadFile/UploadFile.js
@@ -23,84 +23,27 @@ export const UploadFile = ({ setNewMember, NewMember }) => {
       setSelectedFile(undefined);
       return;
     }
-    console.log(e.target.files[0]);
     setNewMember({ ...NewMember, imageFile: e.target.files[0] });
     // I've kept this example simple by using the first image instead of multiple
     setSelectedFile(e.target.files[0]);
   };
   const handleUploadClick = (e) => {
     e.preventDefault();
-    console.log(selectedFile);
     setNewMember({ ...NewMember, imageFile: { url: preview } });
-    console.log("upload");
   };
 
   return (
-    // <div>
-    //   <input type="file" onChange={onSelectFile} />
-    //   {selectedFile && <img src={preview} />}
-    // </div>
     <Form.Group className="mb-3" controlId="formBasicImage">
       <Form.Label>image:</Form.Label>
       <div className="skillsBox d-flex">
         <Form.Control type="file" onChange={onSelectFile} />
-        {/* <div>{file && `${file.name} - ${file.type}`}</div> */}
-        <button onClick={handleUploadClick}>Upload</button>
+        <button className="btn btn-success" onClick={handleUploadClick}>
+          Upload
+        </button>
         {selectedFile && <img className="ImageForm" src={preview} />}
       </div>
     </Form.Group>
   );
 };
-
-// function UploadFile() {
-//   const [file, setFile] = useState();
-
-//   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-//     if (e.target.files) {
-//       setFile(e.target.files[0]);
-//     }
-//   };
-
-//   const handleUploadClick = (e) => {
-//     e.preventDefault();
-//     if (!file) {
-//       return;
-//     }
-//     console.log(file);
-//     // return <img src={file} alt="image" />;
-//     // // 👇 Uploading the file using the fetch API to the server
-//     // fetch('https://httpbin.org/post', {
-//     //   method: 'POST',
-//     //   body: file,
-//     //   // 👇 Set headers manually for single file upload
-//     //   headers: {
-//     //     'content-type': file.type,
-//     //     'content-length': `${file.size}`, // 👈 Headers need to be a string
-//     //   },
-//     // })
-//     //   .then((res) => res.json())
-//     //   .then((data) => console.log(data))
-//     //   .catch((err) => console.error(err));
-//   };
-
-//   return (
-//     // <div>
-//     //   <input type="file" onChange={handleFileChange} />
-
-//     //   <div>{file && `${file.name} - ${file.type}`}</div>
-
-//     //   <button onClick={handleUploadClick}>Upload</button>
-//     // </div>
-//     <Form.Group className="mb-3" controlId="formBasicImage">
-//       <Form.Label>image:</Form.Label>
-//       <div className="skillsBox d-flex">
-//         <Form.Control type="file" onChange={handleFileChange} />
-//         {/* <div>{file && `${file.name} - ${file.type}`}</div> */}
-//         <img src={file} alt="image" />;
-//         <button onClick={handleUploadClick}>Upload</button>
-//       </div>
-//     </Form.Group>
-//   );
-// }
 
 export default UploadFile;
