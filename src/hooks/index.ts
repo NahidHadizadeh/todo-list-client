340b7d09b5ead5cdbbca9f787808d90422b1d03f
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 20:35:26 +0430

    delete files

--- src/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { useColors } from './useColors';
--- src/hooks/useColors.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { colorContext } from '../theme';
-
-export const useColors = () => {
-  const colors = useContext(colorContext);
-
-  return colors;
-};
