24af9035060ffcd548d10d11c027948789ea3883
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-04-24 20:59:06 +0430

    delete commits and logs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-// import AddTodo from "./component/Home";
 import { Route, Routes } from "react-router-dom";
 import CreateTodo from "./component/CreateTodo";
 import EditTodo from "./component/EditTodo";
 import History from "./pages/History";
 import Members from "./pages/Members";
 import Home from "./pages/Home";
-// import NavbarProject from "./component/Navbar";
 import AddMember from "./component/AddMember/AddMember";
 import AddButtonProvider from "./context/AddButtonContext";
-// import useAddButton from "./hooks/AddButton/useAddButton";
 import NotFoundPage from "./pages/NotFound";
 import AllMembersProvider from "./context/MembersContext";
 import AllTasksProvider from "./context/TasksContext";
 import SearchNameProvider from "./context/SearchMember";
 import AllChangingProvider from "./context/HistoryContext";
-// import AddTask from "./component/AddTask/AddTask";
-// import AddTask  from "./"
+
 function App() {
-  // const data = useAddButton();
   return (
-    // <>
-    // {/* <NavbarProject /> */}
-    // <AddButtonProvider></AddButtonProvider>
     <AllChangingProvider>
       <AddButtonProvider>
         <AllMembersProvider>
@@ -32,15 +24,12 @@ function App() {
                   <Route path="createTask" element={<CreateTodo />}></Route>;
                   <Route path="edit" element={<EditTodo />}></Route>;
                 </Route>
-                {/* ;<Route path="/home" element={<Home />} /> */}
                 <Route path="/history" element={<History />} />
                 <Route path="/members/" element={<Members />}>
                   <Route
                     path="addMember"
                     element={
                       <AddMember
-                      // ShowModal={data.ShowModal}
-                      // handleCloseModal={()=>{data.setShowModal(false)}}
                       />
                     }
                   />
@@ -52,7 +41,6 @@ function App() {
         </AllMembersProvider>
       </AddButtonProvider>
     </AllChangingProvider>
-    // </>
   );
 }
 
