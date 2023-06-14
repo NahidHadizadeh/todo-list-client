4f8075eb88c1e8f6c882d510d2b1e0aa18763efd
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-14 11:36:20 +0430

    create "MembersIcon" component

---
+++ src/component/MembersIcon.js
@@ -0,0 +1,43 @@
+import useAllMembers from "../hooks/AllMembers/useAllMembers";
+
+function MembersIcon() {
+  const AllMembers = useAllMembers().AllMembers;
+  return (
+    <div className="listOfMembers">
+      {AllMembers?.map((member, index) => {
+        console.log("first");
+        if (!member.admin)
+          return (
+            <div
+              key={index}
+              className={
+                "displayImage memberImage " +
+                "bgColor" +
+                (Math.floor(Math.random() * 7) + 1)
+              }
+            >
+              {member.name.includes(" ")
+                ? member.name.slice(0, 1).toUpperCase() +
+                  member.name
+                    .slice(
+                      member.name.indexOf(" ") + 1,
+                      member.name.indexOf(" ") + 2
+                    )
+                    .toUpperCase()
+                : member.name.slice(0, 2).toUpperCase()}
+              {/* {member.name.slice(0, 2).toUpperCase()} */}
+
+              {/* <img
+        src="/client/public/user.png"
+        alt={member.title}
+        title={member.title}
+      /> */}
+              {/* <FaUserCircle /> */}
+            </div>
+          );
+      })}
+    </div>
+  );
+}
+
+export default MembersIcon;
