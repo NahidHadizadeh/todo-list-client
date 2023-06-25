7f310506a557ce73437e7374f55cecacd5ee4b48
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-25 12:33:26 +0430

    add member icon componenet for used in other components

---
+++ src/component/MemberIconComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function MemberIconComponent({ member }) {
+  return (
+    <div
+      key={member._id}
+      className={"displayImage memberImage "
+        .concat(" bgColor")
+        .concat(
+          member.bgColor ? member.bgColor : Math.floor(Math.random() * 7) + 1
+        )}
+    >
+      {/* ---------------------- get first char of first name and lastname ,and show those */}
+      {member.name.includes(" ")
+        ? member.name.slice(0, 1).toUpperCase() +
+          member.name
+            .slice(member.name.indexOf(" ") + 1, member.name.indexOf(" ") + 2)
+            .toUpperCase()
+        : member.name.slice(0, 2).toUpperCase()}
+    </div>
+  );
+}
